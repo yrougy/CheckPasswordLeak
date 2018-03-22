e01b0cdbce9c6a380ac27bc9026fbca63c405183
Yves Rougy <yves@MacBook-Pro-de-Yves.local>
2018-03-22 16:52:03 +0100

    Using shasum in MacOS (Darwin) and sha1sum in Linux

--- check_pass.bash
+++ check_pass.bash
@@ -1,12 +1,20 @@
 #/bin/bash
 
+
 URL="https://api.pwnedpasswords.com/range/"
+OS=$(uname -s)
+case $OS in
+	Linux) HASH=sha1sum;;
+	Darwin) HASH=shasum;;
+	*) HASH=sha1sum;;
+esac
+
 
 echo -n "Enter password to check: "
 read -s PASS 
 echo
 
-SHA1PASS=$(echo -n $PASS | shasum | cut -d ' ' -f 1 | tr '[a-f]' '[A-F]')
+SHA1PASS=$(echo -n $PASS | $HASH | cut -d ' ' -f 1 | tr '[a-f]' '[A-F]')
 PASS=''
 HEADSHA="${SHA1PASS:0:5}"
 TAILSHA="${SHA1PASS:5:39}"
