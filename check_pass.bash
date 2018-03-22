b4ce9508523dcd5bb1adc0e6f9d196d77a4ce4e5
Yves Rougy <yves@MacBook-Pro-de-Yves.local>
2018-03-22 16:55:02 +0100

    A bit of cleanup

--- check_pass.bash
+++ check_pass.bash
@@ -2,8 +2,7 @@
 
 
 URL="https://api.pwnedpasswords.com/range/"
-OS=$(uname -s)
-case $OS in
+case $(uname -s) in
 	Linux) HASH=sha1sum;;
 	Darwin) HASH=shasum;;
 	*) HASH=sha1sum;;
@@ -14,11 +13,11 @@ echo -n "Enter password to check: "
 read -s PASS 
 echo
 
-SHA1PASS=$(echo -n $PASS | $HASH | cut -d ' ' -f 1 | tr '[a-f]' '[A-F]')
+SHA1PASS=$(echo -n "$PASS" | "$HASH" | cut -d ' ' -f 1 | tr '[a-f]' '[A-F]')
 PASS=''
 HEADSHA="${SHA1PASS:0:5}"
 TAILSHA="${SHA1PASS:5:39}"
 
-REQUEST=${URL}${HEADSHA}
-curl -s $REQUEST | grep "$TAILSHA" | cut -d : -f 2 | xargs echo Found occurences: 
+REQUEST="${URL}${HEADSHA}"
+curl -s "$REQUEST" | grep "$TAILSHA" | cut -d : -f 2 | xargs echo Found occurences: 
 
